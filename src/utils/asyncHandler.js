6235970d3c61ad5ff3e9047cfaa590a1221a6e9a
Shubh Agrawal <shubhagrawal96345@gmail.com>
2026-03-01 11:05:15 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,29 @@
+const asyncHandler = (requstHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(err))
+    }
+}
+
+
+
+
+export {asyncHandler}
+
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
