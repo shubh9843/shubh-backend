93c7540d9149d497eba560cd2a1aa543749c7150
Shubh Agrawal <shubhagrawal96345@gmail.com>
2026-03-22 01:01:44 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -11,7 +11,9 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extented: true, limit: "16kb"}))
+
 app.use(express.static("public"))
+
 app.use(cookieParser())
 
 
--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ dotenv.config({
 })
 
 connectDB()
-
 .then(() => {
     app.listen(process.env.PORT || 8000, () =>{
         console.log (`Server is running at port: ${process.env.PORT}`);
